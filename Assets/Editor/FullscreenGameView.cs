9f0db1e416ced5c32766191ae458e3149fdaf307
RiftworksStudios-25 <riftworksstudios@gmail.com>
2026-08-05 17:04:25 -0700

    fullscreen game view

---
+++ Assets/Editor/FullscreenGameView.cs
@@ -0,0 +1,49 @@
+#if UNITY_EDITOR
+
+using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public static class FullscreenGameView
+{
+    static readonly Type GameViewType = Type.GetType("UnityEditor.GameView,UnityEditor");
+    static readonly PropertyInfo ShowToolbarProperty = GameViewType?.GetProperty("showToolbar", BindingFlags.Instance | BindingFlags.NonPublic);
+    static readonly object False = false; 
+
+    static EditorWindow instance;
+
+    [MenuItem("Window/General/Game (Fullscreen) %#&2", priority = 2)]
+    public static void Toggle()
+    {
+        if (GameViewType == null)
+        {
+            Debug.LogError("GameView type not found.");
+            return;
+        }
+
+        if (instance != null)
+        {
+            instance.Close();
+            instance = null;
+        }
+        else
+        {
+
+            instance = (EditorWindow)ScriptableObject.CreateInstance(GameViewType);
+
+            ShowToolbarProperty?.SetValue(instance, False);
+
+
+            var desktopResolution = new Vector2(Screen.currentResolution.width, Screen.currentResolution.height);
+            var fullscreenRect = new Rect(Vector2.zero, desktopResolution);
+            
+            instance.position = fullscreenRect;
+
+            instance.ShowPopup(); 
+            instance.Focus();
+        }
+    }
+}
+
+#endif
